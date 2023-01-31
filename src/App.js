5dc1a271e1bf108ef44e5c3651c012e09e6a2793
Adiba Belayete <adiba.du.bd@gmail.com>
2023-01-31 00:53:49 +0000

    styling completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import React, { useState } from "react";
 import babyNames from "./data/babyNamesData.json";
+import Heading from "./component/Heading";
 
 import BabyNamesForMainSection from "./component/BabyNamesMainSection";
 
@@ -9,11 +10,11 @@ function App() {
   const sortedName = babyNames.sort((a, b)=>a.name.localeCompare(b.name) )
   // console.log(sortedName)
   return (
-    <div >
-      <header>
-        <h1> Baby Name Picker </h1>
-      </header>
-      <BabyNamesForMainSection babyNames={babyNames}/>
+    <div className="App">
+      <Heading />
+        <h1> Choose a name for you future... </h1>
+      
+      <BabyNamesForMainSection babyNames={babyNames} />
     </div>
   );
 }
---
+++ src/component/Heading.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Heading() {
+    return (
+      <header className="App-header">
+        <img
+          className="App-logo "
+          src="https://freesvg.org/img/baby_winter_wear.png"
+          alt="baby logo"
+        />
+        Our future ...
+      </header>
+    ); }
+export default Heading
